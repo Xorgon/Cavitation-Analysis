cf0efd0fc574269a9a3b8b2eac48a4337e215941
Elijah Andrews <eda1g15@soton.ac.uk>
2019-11-06 19:11:04 +0000

    Recreated surface_integral.py with improved method.

--- numerical/testing/surface_integral.py
+++ numerical/testing/surface_integral.py
@@ -1,8 +1,8 @@
-from scipy.integrate import quad, dblquad
 import numpy as np
-import matplotlib.pyplot as plt
 import itertools
-import math
+import matplotlib.pyplot as plt
+from datetime import datetime
+from common.util.plotting_utils import plot_3d_point_sets
 
 
 def get_u(r):
@@ -12,91 +12,92 @@ def get_u(r):
     return - r_hat / (4 * np.pi * length ** 2)
 
 
-def get_r(t, p, d, radius):
-    return np.array([
-        radius * np.cos(t) * np.cos(p),
-        d + radius * np.sin(t),
-        radius * np.cos(t) * np.sin(p)
-    ])
+def get_avg_u(bubble_pos, sinks, radius, n=int(1e5)):
+    vels = []
+    positions = []
+    offsets = np.random.randn(3, n)
+    offsets *= radius / np.linalg.norm(offsets, axis=0)
+    offsets = offsets.T
+    for offset in offsets:
+        pos_1 = bubble_pos + offset
+        pos_2 = bubble_pos - offset
+
+        vel_1 = np.array([0., 0., 0.])
+        vel_2 = np.array([0., 0., 0.])
+
+        for sink in sinks:
+            vel_1 += get_u(pos_1 - sink)
+            vel_2 += get_u(pos_2 - sink)
+        positions.append(pos_1)
+        vels.append(vel_1)
+        vels.append(vel_2)
+    # plot_3d_point_sets([positions])
+    return np.mean(vels, axis=0)
 
 
-def get_vel(t, p, d, radius):
-    r = get_r(t, p, d, radius)
-    return get_u(r)
+# theta_b = np.pi / 4
+# b = np.array([np.cos(theta_b), np.sin(theta_b), 0])
+# sink_pos = np.array([[-b[0], b[1], 0],
+#                      [-b[0], -b[1], 0],
+#                      [b[0], -b[1], 0]])
+# v = get_avg_u(b, sink_pos, 0.5)
+# print(b)
+# print(sink_pos)
+# print(v)
+# print(np.pi / (- np.arctan2(v[1], v[0]) - np.pi / 2))
 
+# plt.scatter(b[0], b[1])
+# plt.scatter(sink_pos[:, 0], sink_pos[:, 1])
+# plt.axhline(0)
+# plt.axvline(0)
+# plt.show()
 
-# def get_vel_x(t, p, d, radius):
-#     return get_vel(t, p, d, radius)[0]
+# radii = [0.1, 0.3, 0.5]
+# for radius in radii:
+#     print(f"radius = {radius}")
+#     theta_bs = np.linspace(np.arcsin(radius), np.pi / 2 - np.arcsin(radius), 16)
+#     theta_js = []
+#     for theta_b in theta_bs:
+#         print(f"    theta_b = {theta_b}")
+#         b = np.array([np.cos(theta_b), np.sin(theta_b), 0])
+#         sink_pos = [[-b[0], b[1], 0],
+#                     [-b[0], -b[1], 0],
+#                     [b[0], -b[1], 0]]
+#         v = get_avg_u(b, sink_pos, radius, n=int(1e5))
+#         theta_js.append(- np.arctan2(v[1], v[0]) - np.pi / 2)
+#     plt.plot(theta_bs, theta_js)
+#
+# plt.axhline(np.pi / 4)
+# plt.axvline(np.pi / 4)
+# plt.show()
 
+theta_b = np.pi / 3
+b = np.array([np.cos(theta_b), np.sin(theta_b), 0])
+sink_pos = [[-b[0], b[1], 0],
+            [-b[0], -b[1], 0],
+            [b[0], -b[1], 0]]
+radii = np.linspace(0.01, np.sin(theta_b), 32)
+angles = []
+for radius in radii:
+    print(radius)
+    v = get_avg_u(b, sink_pos, radius, n=int(1e5))
+    angles.append(- np.arctan2(v[1], v[0]) - np.pi / 2)
 
-def get_vel_y(t, p, d, radius):
-    """ Velocity in the axial direction (positive is from the sink to the bubble) """
-    return get_vel(t, p, d, radius)[1]
+plt.plot(radii, angles)
+plt.show()
 
-
-# def get_vel_z(t, p, d, radius):
-#     return get_vel(t, p, d, radius)[2]
-
-
-if __name__ == "__main__":
-    h = 0.05
-    bubble_pos = np.array([1, 2, 0])
-    bubble_radius = min([i for i in bubble_pos if i != 0]) / 1.5
-
-    sink_pos = [[-bubble_pos[0], bubble_pos[1], 0],
-                [-bubble_pos[0], -bubble_pos[1], 0],
-                [bubble_pos[0], -bubble_pos[1], 0]]
-    # sink_pos = [[bubble_pos[0], - bubble_pos[1], 0]]
-
-    total_vel = np.array([0., 0., 0.])
-    test_vel = np.array([0., 0., 0.])
-    center_vel = np.array([0., 0., 0.])
-
-    ts = np.arange(0, np.pi, h)
-    ps = np.arange(0, 2 * np.pi, h)
-    for sink in sink_pos:
-        # Surface integral
-        sink = np.array(sink)
-        r = sink - bubble_pos
-        dist = np.linalg.norm(r)
-        r_hat = r / dist
-
-        # get_vel_y is the axial velocity component for the sink-bubble combination.
-        # Multiply by the unit direction vector from the bubble to the sink to get
-        total_vel -= r_hat * dblquad(get_vel_y, 0, np.pi, lambda _: 0, lambda _: 2 * np.pi,
-                                     args=(dist, bubble_radius))[0]
-
-        # Center
-        center_vel -= get_u(r)
-
-        # Crude approximation to check integration
-        count = 0
-        last_print = 0
-        for t, p in itertools.product(ts, ps):
-            if count - last_print > 0.05 * len(ts) * len(ps):
-                print(f"{count / (len(ts) * len(ps)) * 100:.2f}%")
-                last_print = count
-            count += 1
-            v = r_hat * np.linalg.norm(get_vel(t, p, dist, bubble_radius))
-            test_vel += v
-
-    print(total_vel)
-    total_vel /= 4 * np.pi * bubble_radius ** 2
-
-    test_vel *= (4 * np.pi * bubble_radius ** 2) / (len(ts) * len(ps))  # Area per point
-    test_vel /= 4 * np.pi * bubble_radius ** 2
-    print(test_vel)
-
-    print(center_vel * 4 * np.pi * bubble_radius ** 2)
-
-    print(f"Surface integration:\n"
-          f"    Velocity: {total_vel}\n"
-          f"    Angle: {math.atan2(total_vel[1], total_vel[0])}")
-
-    print(f"Test:\n"
-          f"    Velocity: {test_vel}\n"
-          f"    Angle: {math.atan2(test_vel[1], test_vel[0])}")
-
-    print(f"Bubble center:\n"
-          f"    Velocity: {center_vel}\n"
-          f"    Angle: {math.atan2(center_vel[1], center_vel[0])}")
+# theta_b = np.pi / 4
+# b = np.array([np.cos(theta_b), np.sin(theta_b), 0])
+# sink_pos = [[-b[0], b[1], 0],
+#             [-b[0], -b[1], 0],
+#             [b[0], -b[1], 0]]
+# hs = np.logspace(np.log10(0.005), np.log10(0.1), 16)
+# angles = []
+# for h in hs:
+#     print(h)
+#     v = get_avg_u(b, sink_pos, 0.5, h=h)
+#     angles.append(- np.arctan2(v[1], v[0]) - np.pi / 2)
+#
+# plt.axhline(np.pi / 4)
+# plt.plot(hs, angles)
+# plt.show()
