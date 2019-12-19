f551e3b80a23dd6f2499ad8da59f35828770f5b6
Elijah Andrews <eda1g15@soton.ac.uk>
2019-12-19 16:01:59 +0000

    Improved efficiency of automatic data analysis script.

--- experimental/analysis/analyse_data.py
+++ experimental/analysis/analyse_data.py
@@ -6,21 +6,23 @@ import os
 
 import experimental.util.analysis_utils as au
 import experimental.util.file_utils as file
+import multiprocessing
 from PyQt5.QtGui import QGuiApplication
 
-reanalyse = True
+if __name__ == "__main__":
+    reanalyse = True
 
-w = QGuiApplication([])
-dir_path = file.select_dir()
-dirs_to_analyse = []
-for root, dirs, files in os.walk(dir_path):
-    if "index.csv" in files and ("readings_dump.csv" not in files or reanalyse):
-        dirs_to_analyse.append(root)
+    w = QGuiApplication([])
+    dir_path = file.select_dir()
+    w.exit()
+    dirs_to_analyse = []
+    for root, dirs, files in os.walk(dir_path):
+        if "index.csv" in files and ("readings_dump.csv" not in files or reanalyse):
+            dirs_to_analyse.append(root + "/")
 
-for this_dir in dirs_to_analyse:
-    print(f"Analysing {this_dir}")
-    au.analyse_series(this_dir + "/")
-    try:
-        au.flag_invalid_readings(this_dir)
-    except ValueError:
-        print(f"Could not flag invalid readings in {this_dir}")
+    pool = multiprocessing.Pool()
+    pool.map(au.analyse_series, dirs_to_analyse)
+
+    # for this_dir in dirs_to_analyse:
+    #     print(f"Analysing {this_dir}")
+    #     au.analyse_series(this_dir + "/")
