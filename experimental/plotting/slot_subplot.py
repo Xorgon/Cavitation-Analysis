63bfb05dddcce250ae56903c441ce6d5b07f1bac
Elijah Andrews <eda1g15@soton.ac.uk>
2019-12-19 16:03:16 +0000

    Improved functionality of slot_subplot.

--- experimental/plotting/slot_subplot.py
+++ experimental/plotting/slot_subplot.py
@@ -3,8 +3,9 @@ from experimental.plotting.analyse_slot import analyse_slot
 import itertools
 import common.util.plotting_utils as plt_util
 
-num_cols = 2
+num_cols = 3
 num_rows = 3
+num_plots = 7
 
 plt_util.initialize_plt()
 
@@ -15,8 +16,9 @@ config = {
     "plot_fits": False,
     "skip_bad_data": True,
     "plot_means": False,
-    "labelled": True,
+    "labelled": False,
     "label_tags": False,
+    "colours": True,
     "error_bars": True,
     "show_title": False,
     "do_shift": True,
@@ -40,16 +42,19 @@ fig_height = ax_width * num_rows + bottom_padding + top_padding + (num_rows - 1)
 fig, axes = plt.subplots(num_rows, num_cols, sharex='none', sharey="all", figsize=(fig_width, fig_height))
 labels = "abcdefghijk"
 for j, i in itertools.product(range(num_rows), range(num_cols)):
-    axes[j, i].tick_params(axis='both', which='major', labelsize=10)
-    analyse_slot(axes[j, i], set_y_label=(i == 0), set_x_label=(j == num_rows - 1),
-                 use_defaults=False, config=config, num_series=1)
-    axes[j, i].xaxis.label.set_size(10)
-    axes[j, i].yaxis.label.set_size(10)
-    text_pos = ((left_padding + i * (h_spacing + ax_width) + 0.075) / fig_width,
-                (bottom_padding + (num_rows - j) * (ax_width + v_spacing) - 0.1 - v_spacing) / fig_height)
-    axes[j, i].annotate("$" + f"{labels[i + num_cols * j]}" + ")$", xy=(0, 0), xytext=text_pos,
-                        textcoords='figure fraction',
-                        horizontalalignment='left', verticalalignment='top')
+    if 3 * j + i >= num_plots:
+        axes[j, i].set_visible(False)
+    else:
+        axes[j, i].tick_params(axis='both', which='major', labelsize=10)
+        analyse_slot(axes[j, i], set_y_label=(i == 0), set_x_label=(j == num_rows - 1),
+                     use_defaults=False, config=config, num_series=1)
+        axes[j, i].xaxis.label.set_size(10)
+        axes[j, i].yaxis.label.set_size(10)
+        text_pos = ((left_padding + i * (h_spacing + ax_width) + 0.075) / fig_width,
+                    (bottom_padding + (num_rows - j) * (ax_width + v_spacing) - 0.1 - v_spacing) / fig_height)
+        axes[j, i].annotate("$" + f"{labels[i + num_cols * j]}" + ")$", xy=(0, 0), xytext=text_pos,
+                            textcoords='figure fraction',
+                            horizontalalignment='left', verticalalignment='top')
 
 plt.subplots_adjust(left=left_padding / fig_width,
                     right=(fig_width - right_padding) / fig_width,
