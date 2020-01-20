c75bf48bd6a2ce5770da66ebc128a44296eae266
Elijah Andrews <eda1g15@soton.ac.uk>
2020-01-20 17:28:10 +0000

    Added file and directory selection utilities to common file_utils.

--- common/util/file_utils.py
+++ common/util/file_utils.py
@@ -1,3 +1,6 @@
+from PyQt5.QtWidgets import QFileDialog, QApplication
+
+
 def array_to_csv(path, filename, array):
     file = open(path + filename, "w")
     for entry in array:
@@ -7,3 +10,17 @@ def array_to_csv(path, filename, array):
         line += "\n"
         file.write(line)
     file.close()
+
+
+def select_file(start_path, create_window=True):
+    if create_window:
+        window = QApplication([])
+    file_path = str(QFileDialog.getOpenFileName(None, "Select File", start_path)[0])
+    return file_path
+
+
+def select_dir(start_path, create_window=True):
+    if create_window:
+        window = QApplication([])
+    dir_path = str(QFileDialog.getExistingDirectory(None, "Select Directory", start_path)) + "/"
+    return dir_path
