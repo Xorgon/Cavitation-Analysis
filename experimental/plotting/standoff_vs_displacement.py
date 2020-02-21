fd12993f77daba6ff08948fe00c6a782335a4f23
Elijah Andrews <eda1g15@soton.ac.uk>
2020-02-21 19:50:29 +0000

    Modified standoff_vs_displacement graphing.

--- experimental/plotting/standoff_vs_displacement.py
+++ experimental/plotting/standoff_vs_displacement.py
@@ -62,78 +62,94 @@ for i, dir_path in enumerate(dirs):
 
 initialize_plt(font_size=14, line_scale=2)
 plt.figure()
-plt.scatter(np.divide(qs, radii), np.divide(displacements, radii),
-            c=np.log(np.abs(2 * np.divide(ps, ws))) > 1, marker=".", cmap=plt.cm.get_cmap('brg'))
+standoffs = np.divide(qs, radii)
+fit_params = np.polyfit([i for i, p, w in zip(np.log(standoffs), ps, ws) if abs(p) > w],
+                        [i for i, p, w in zip(np.log(np.divide(displacements, radii)), ps, ws) if abs(p) > w],
+                        1)
+# fit_params = np.polyfit(np.log(standoffs), np.log(np.divide(displacements, radii)), 1)
+fit_standoffs = np.linspace(min(standoffs), max(standoffs), 100)
+fit_disps = np.exp(fit_params[1]) * np.power(fit_standoffs, fit_params[0])
+fit_label = "$\\Delta / R = %.3f (q / R)^{%.3f}$ (fitted)" % (np.exp(fit_params[1]), fit_params[0])
+
+num_fact = 4.5
+num_disps = num_fact * np.power(fit_standoffs, -2)
+
+plt.scatter(standoffs, np.divide(displacements, radii),
+            c=np.log(np.abs(2 * np.divide(ps, ws))), marker=".")
+plt.plot(fit_standoffs, fit_disps, label=fit_label)
+plt.plot(fit_standoffs, num_disps, label=("$v = %.3f q^{-2}$" % num_fact))
 plt.colorbar(label="$log(|\\bar{p}|)$")
 plt.xlabel("$q / R$")
-plt.ylabel("$\Delta / R$ = displacement / radius")
-plt.xscale('log')
-plt.yscale('log')
-plt.tight_layout()
-
-plt.figure()
-plt.scatter(np.divide(qs, radii), avg_vels,
-            c=np.log(np.abs(2 * np.divide(ps, ws))), marker=".", cmap=plt.cm.get_cmap('brg'))
-plt.xlabel("$q / R$")
-plt.ylabel("Average translation velocity")
-plt.xscale('log')
-plt.yscale('log')
-plt.tight_layout()
-
-plt.figure()
-fit_params = np.polyfit(np.log(zetas), np.log(np.divide(displacements, radii)), 1)
-fit_zetas = np.linspace(min(zetas), max(zetas), 100)
-fit_disps = np.exp(fit_params[1]) * np.power(fit_zetas, fit_params[0])
-fit_label = "$%.3f \\zeta^{%.3f}$ (fitted)" % (np.exp(fit_params[1]), fit_params[0])
-
-sup_pow_disps = 2.5 * np.power(fit_zetas, 0.6)
-eye_disps = 2.8 * np.power(fit_zetas, 0.42)
-
-plt.plot(fit_zetas, fit_disps, label=fit_label)
-plt.plot(fit_zetas, sup_pow_disps, label="$2.5 \\zeta^{0.6}$ (Supponen et al. 2016)")
-plt.plot(fit_zetas, eye_disps, color="purple", label="$2.8 \\zeta^{0.42}$ (by eye)")
-plt.scatter(zetas, np.divide(displacements, radii),
-            c=np.log(np.abs(2 * np.divide(ps, ws))), marker=".", cmap=plt.cm.get_cmap('brg'))
-plt.colorbar(label="$log(|\\bar{p}|)$")
-plt.xlabel("$\\zeta$ (Supponen et al. 2016)")
 plt.ylabel("$\Delta / R$ = displacement / radius")
 plt.xscale('log')
 plt.yscale('log')
 plt.legend()
 plt.tight_layout()
 
-plt.figure()
-fit_params = np.polyfit(np.log(zetas), np.log(np.divide(sup_disps, radii)), 1)
-fit_zetas = np.linspace(min(zetas), max(zetas), 100)
-fit_disps = np.exp(fit_params[1]) * np.power(fit_zetas, fit_params[0])
-fit_label = "$%.3f \\zeta^{%.3f}$ (fitted)" % (np.exp(fit_params[1]), fit_params[0])
+# plt.figure()
+# plt.scatter(np.divide(qs, radii), avg_vels,
+#             c=np.log(np.abs(2 * np.divide(ps, ws))), marker=".", cmap=plt.cm.get_cmap('brg'))
+# plt.xlabel("$q / R$")
+# plt.ylabel("Average translation velocity")
+# plt.xscale('log')
+# plt.yscale('log')
+# plt.tight_layout()
+#
+# plt.figure()
+# fit_params = np.polyfit(np.log(zetas), np.log(np.divide(displacements, radii)), 1)
+# fit_zetas = np.linspace(min(zetas), max(zetas), 100)
+# fit_disps = np.exp(fit_params[1]) * np.power(fit_zetas, fit_params[0])
+# fit_label = "$%.3f \\zeta^{%.3f}$ (fitted)" % (np.exp(fit_params[1]), fit_params[0])
+#
+# sup_pow_disps = 2.5 * np.power(fit_zetas, 0.6)
+# eye_disps = 2.8 * np.power(fit_zetas, 0.42)
+#
+# plt.plot(fit_zetas, fit_disps, label=fit_label)
+# plt.plot(fit_zetas, sup_pow_disps, label="$2.5 \\zeta^{0.6}$ (Supponen et al. 2016)")
+# plt.plot(fit_zetas, eye_disps, color="purple", label="$2.8 \\zeta^{0.42}$ (by eye)")
+# plt.scatter(zetas, np.divide(displacements, radii),
+#             c=np.log(np.abs(2 * np.divide(ps, ws))), marker=".", cmap=plt.cm.get_cmap('brg'))
+# plt.colorbar(label="$log(|\\bar{p}|)$")
+# plt.xlabel("$\\zeta$ (Supponen et al. 2016)")
+# plt.ylabel("$\Delta / R$ = displacement / radius")
+# plt.xscale('log')
+# plt.yscale('log')
+# plt.legend()
+# plt.tight_layout()
+#
+# plt.figure()
+# fit_params = np.polyfit(np.log(zetas), np.log(np.divide(sup_disps, radii)), 1)
+# fit_zetas = np.linspace(min(zetas), max(zetas), 100)
+# fit_disps = np.exp(fit_params[1]) * np.power(fit_zetas, fit_params[0])
+# fit_label = "$%.3f \\zeta^{%.3f}$ (fitted)" % (np.exp(fit_params[1]), fit_params[0])
+#
+# sup_pow_disps = 2.5 * np.power(fit_zetas, 0.6)
+#
+# plt.plot(fit_zetas, fit_disps, label=fit_label)
+# plt.plot(fit_zetas, sup_pow_disps, label="$2.5 \\zeta^{0.6}$ (Supponen et al. 2016)")
+# plt.scatter(zetas, np.divide(displacements, radii), marker="1", c="k", label="Original displacement")
+# plt.scatter(zetas, np.divide(sup_disps, radii),
+#             # c=times)
+#             c=np.log(np.abs(2 * np.divide(ps, ws))), marker=".", cmap=plt.cm.get_cmap('brg'),
+#             label="Supponen displacement")
+#
+# # WARNING: Do not have LaTeX plotting on if you want to use this
+# # for k in zip(zetas, np.divide(sup_disps, radii), idxs):
+# #     plt.annotate(f'{k[2] + 1}', xy=[k[0], k[1]], textcoords='data')
+#
+# plt.colorbar(label="$log(|\\bar{p}|)$")
+# plt.xlabel("$\\zeta$ (Supponen et al. 2016)")
+# plt.ylabel("Normalised bubble centroid displacement = $\Delta z / R_0$")
+# plt.xscale('log')
+# plt.yscale('log')
+# plt.legend()
+# plt.tight_layout()
+#
+# plt.figure()
+# plt.scatter(radii, displacements, c=qs, marker="s")
+# plt.colorbar(label="$q (mm)$")
+# plt.xlabel("$R$ = Radius (mm)")
+# plt.ylabel("$\Delta$ = displacement (mm)")
+# plt.tight_layout()
 
-sup_pow_disps = 2.5 * np.power(fit_zetas, 0.6)
-
-plt.plot(fit_zetas, fit_disps, label=fit_label)
-plt.plot(fit_zetas, sup_pow_disps, label="$2.5 \\zeta^{0.6}$ (Supponen et al. 2016)")
-plt.scatter(zetas, np.divide(displacements, radii), marker="1", c="k", label="Original displacement")
-plt.scatter(zetas, np.divide(sup_disps, radii),
-            # c=times)
-            c=np.log(np.abs(2 * np.divide(ps, ws))), marker=".", cmap=plt.cm.get_cmap('brg'),
-            label="Supponen displacement")
-
-# WARNING: Do not have LaTeX plotting on if you want to use this
-# for k in zip(zetas, np.divide(sup_disps, radii), idxs):
-#     plt.annotate(f'{k[2] + 1}', xy=[k[0], k[1]], textcoords='data')
-
-plt.colorbar(label="$log(|\\bar{p}|)$")
-plt.xlabel("$\\zeta$ (Supponen et al. 2016)")
-plt.ylabel("Normalised bubble centroid displacement = $\Delta z / R_0$")
-plt.xscale('log')
-plt.yscale('log')
-plt.legend()
-plt.tight_layout()
-
-plt.figure()
-plt.scatter(radii, displacements, c=qs, marker="s")
-plt.colorbar(label="$q (mm)$")
-plt.xlabel("$R$ = Radius (mm)")
-plt.ylabel("$\Delta$ = displacement (mm)")
-plt.tight_layout()
 plt.show()
